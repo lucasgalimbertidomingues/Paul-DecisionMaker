3629ada918ce5e132a09dfae8133d743bcd455ed
Lucas Galimberti Domingues <lucas.domingues@softplan.com.br>
2017-09-28 20:17:23 -0300

    Initial project commit

---
+++ app/src/main/java/dominguessolutions/paul_decisionmaker/decisionmaker/view/MainActivity.java
@@ -0,0 +1,15 @@
+package dominguessolutions.paul_decisionmaker.decisionmaker.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dominguessolutions.paul_decisionmaker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
